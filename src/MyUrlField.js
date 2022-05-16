25e4d88a2be3619e8de1246fd8112f80ef1c0d0b
Jaewan Park <hueypark@line.games>
2022-05-17 07:59:47 +0900

    커스텀 URL 필드에 디자인 수정

--- src/MyUrlField.js
+++ src/MyUrlField.js
@@ -1,12 +1,15 @@
 import * as React from "react";
 import { useRecordContext } from 'react-admin';
+import { Link } from '@mui/material';
+import LaunchIcon from '@mui/icons-material/Launch';
 
 const MyUrlField = ({ source }) => {
     const record = useRecordContext();
     return record ? (
-        <a href={record[source]}>
+        <Link href={record[source]} sx={{ textDecoration: 'none' }}>
             {record[source]}
-        </a>
+            <LaunchIcon sx={{ width: '0.5em', height: '0.5em', paddingLeft: 2 }} />
+        </Link>
     ) : null;
 }
 
