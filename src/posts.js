e6583f68becab3648945dfd368187684a0b1b9ba
Jaewan Park <hueypark@line.games>
2022-05-17 23:50:09 +0900

    PostEdit, PostCreate 추가

--- src/posts.js
+++ src/posts.js
@@ -1,5 +1,17 @@
 import * as React from "react";
-import { List, Datagrid, TextField, ReferenceField, EditButton } from 'react-admin';
+import {
+    List,
+    Datagrid,
+    TextField,
+    ReferenceField,
+    EditButton,
+    Edit,
+    Create,
+    SimpleForm,
+    ReferenceInput,
+    SelectInput,
+    TextInput,
+} from 'react-admin';
 
 export const PostList = () => (
     <List>
@@ -12,4 +24,29 @@ export const PostList = () => (
             <EditButton />
         </Datagrid>
     </List>
+);
+
+export const PostEdit = () => (
+    <Edit>
+        <SimpleForm>
+            <TextInput disabled source="id" />
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const PostCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Create>
 );
